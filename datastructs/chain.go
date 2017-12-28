67b2ecc33faca6534bb21291f138a45575da7700
aniketalshi <aniketalshi@gmail.com>
2017-12-27 21:02:38 -0800

    Adds boltDB persistance for chain.
    Struct of blockchain is modified to include boltDB reference and last
    Hash as tip.

--- datastructs/chain.go
+++ datastructs/chain.go
@@ -1,21 +1,83 @@
 package blockchain
 
+import (
+	"log"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+const dbFile = "blocks.db"
+const blocksBucket = "Blocks"
+
+// BlockChain defines link of blocks
 type BlockChain struct {
-	Blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
 
 // AddBlock appends the block to end of blockchain
 func (b *BlockChain) AddBlock(data string) {
+	// construct new block and prev hash will be current tip of db
+	block := NewBlock(data, b.tip)
 
-	prevHash := b.Blocks[len(b.Blocks)-1].Hash
-	block := NewBlock(data, prevHash)
-	b.Blocks = append(b.Blocks, block)
+	err := b.db.Update(func(tx *bolt.Tx) error {
+		bckt := tx.Bucket([]byte(blocksBucket))
+		if err := bckt.Put(block.Hash, block.Serialize()); err != nil {
+			return err
+		}
+		if err := bckt.Put([]byte("l"), block.Hash); err != nil {
+			return err
+		}
+		b.tip = block.Hash
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal("AddBlock :", err)
+	}
 }
 
+//GenerateGenesisBlock creates first BlockChain block
 func GenerateGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
 }
 
+//NewBlockChain constructs a new block chain
 func NewBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{GenerateGenesisBlock()}}
+	//return &BlockChain{[]*Block{GenerateGenesisBlock()}}
+
+	var tip []byte
+
+	db, err := bolt.Open(dbFile, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		// if we already have bucket with blocks
+		if b == nil {
+			bl := GenerateGenesisBlock()
+
+			bckt, _ := tx.CreateBucket([]byte(blocksBucket))
+			if newerr := bckt.Put(bl.Hash, bl.Serialize()); err != nil {
+				return newerr
+			}
+			if newerr := bckt.Put([]byte("l"), bl.Hash); err != nil {
+				return newerr
+			}
+			tip = bl.Hash
+		} else {
+			// set tip to last existing hash
+			tip = b.Get([]byte("l"))
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal("NewBlockChain :", err)
+	}
+	return &BlockChain{tip, db}
 }
--- main.go
+++ main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"blockchain/datastructs"
-	"fmt"
 )
 
 func main() {
 	bc := blockchain.NewBlockChain()
 	bc.AddBlock("BTC 1")
 	bc.AddBlock("BTC 2")
-
-	for _, block := range bc.Blocks {
-		fmt.Printf("Prev Hash : %x\n", block.PrevBlockHash)
-		fmt.Printf("Data : %s\n", block.Data)
-		fmt.Printf("Hash : %x\n", block.Hash)
-		fmt.Println()
-	}
 }
